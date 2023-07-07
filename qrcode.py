868920eb51335d4d66777b0a67279b2520a68557
黃(黄)文定-ㄏㄨㄤˊ ㄨㄣˊㄉㄧㄥˋ <91007091+HuangWending@users.noreply.github.com>
2023-07-07 16:16:59 +0800

    Add files via upload

---
+++ qrcode.py
@@ -0,0 +1,20 @@
+import qrcode
+from PIL import Image
+
+def generate_qrcode(text, filename):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(filename)
+
+    
+    img.show()
+
+generate_qrcode("黃文定", "qrcode.png")
